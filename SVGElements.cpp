174210a14d9644186b9cc852a5743dd5da40d3b3
João Lopes <joaomiguel20059@gmail.com>
2024-05-14 16:23:43 +0100

    fixed rectangle function (finished), jl and lm

--- SVGElements.cpp
+++ SVGElements.cpp
@@ -17,58 +17,64 @@ namespace svg
     {
         img.draw_ellipse(center, radius, fill);
     }
-    //############################################################################################################
-    // @todo provide the implementation of SVGElement derived classes
-    // HERE -->
-            //Retângulo (LM)
-    Rectangle::Rectangle(const Point &upperL, 
+    // ############################################################################################################
+    //  @todo provide the implementation of SVGElement derived classes
+    //  HERE -->
+    // Retângulo (LM)
+    Rectangle::Rectangle(const Point &upperL,
                          const int width,
                          const int height,
-                         const Color &fill )
-        : Polygon({upperL, 
-                  {upperL.x + width, upperL.y}, 
-                  {upperL.x, upperL.y - height}, 
-                  {upperL.x + width, upperL.y - height}},
-                   fill),
-      fill(fill), width(width), height(height), upperL(upperL) 
-      {}
-    void Rectangle::draw(PNGImage &img) const {
-    std::vector<Point> rectPoints = get_rect_coordinates();
-    img.draw_polygon(rectPoints, fill); 
-}
-    //############################################################################################################
-            //Polígono (LM)
-    Polygon::Polygon(std::vector <svg::Point> all_points,
-                     const svg::Color &fill)
-                : fill(fill), all_points(all_points)
-    {    
+                         const Color &fill)
+        : Polygon({upperL,
+                   {upperL.x + width, upperL.y},
+                   {upperL.x, upperL.y - height},
+                   {upperL.x + width, upperL.y - height}},
+                  fill),
+          fill(fill), width(width), height(height), upperL(upperL)
+    {
     }
-    void Polygon::draw(PNGImage &img) const 
-        {
-            img.draw_polygon(all_points, fill);
-        }
-        
-            //Circulo
+    void Rectangle::draw(PNGImage &img) const
+    {
+        std::vector<Point> rectPoints = get_rect_coordinates();
+        img.draw_polygon(rectPoints, fill);
+    }
+    // ############################################################################################################
+    // Polígono (LM)
+    Polygon::Polygon(std::vector<svg::Point> all_points,
+                     const svg::Color &fill)
+        : fill(fill), all_points(all_points)
+    {
+    }
+    void Polygon::draw(PNGImage &img) const
+    {
+        img.draw_polygon(all_points, fill);
+    }
+
+    // Circulo
     Circle::Circle(const Color &fill,
-                    const Point &center,
-                    const int &radius)
+                   const Point &center,
+                   const int &radius)
         : Ellipse(fill, center, {radius, radius})
     {
     }
     void Circle::draw(PNGImage &img) const
     {
-        //img.draw_ellipse(center, radius, fill);
+       // img.draw_ellipse(center, radius, fill);
     }
-    Line::Line(double x1, double y1, double x2, double y2, const std::string& stroke): x1(x1), y1 (y1), x2(x2), y2(y2), stroke(stroke){}
+    /* Line::Line(double x1, double y1, double x2, double y2, const std::string &stroke) : x1(x1), y1(y1), x2(x2), y2(y2), stroke(stroke) {}
     Line::~Line() {}
 
-    void Line::draw(PNGImage& image)const {
-        image.drawLine(x1, y1, x2, y2, stroke);
+    void Line::draw(PNGImage &image) const
+    {
+        // image.drawLine(x1, y1, x2, y2, stroke);
     }
-    Polyline::Polyline(const std::vector<Point>& points, const std::string& stroke):points(points), stroke(stroke){}
-    Polyline::~Polyline(){}
-    void Polyline::draw(PNGImage& image) const {
-        for (size_t i= 0; i <points.size() -1; ++i) {
-            image.drawLine(points[i].x, points.y, points[i+1].x, points[i+1].y, stroke);
+    Polyline::Polyline(const std::vector<Point> &points, const std::string &stroke) : points(points), stroke(stroke) {}
+    Polyline::~Polyline() {}
+    void Polyline::draw(PNGImage &image) const
+    {
+        for (size_t i = 0; i < points.size() - 1; ++i)
+        {
+            // image.drawLine(points[i].x, points.y, points[i+1].x, points[i+1].y, stroke);
         }
+    } */
 }
--- SVGElements.hpp
+++ SVGElements.hpp
@@ -5,7 +5,7 @@
 #include "Color.hpp"
 #include "Point.hpp"
 #include "PNGImage.hpp"
-#include<vector> //usado (pelo menos) na classe Polygon
+#include <vector> //usado (pelo menos) na classe Polygon
 
 namespace svg
 {
@@ -39,8 +39,8 @@ namespace svg
         Point center;
         Point radius;
     };
-    
-    class Circle : public Ellipse 
+
+    class Circle : public Ellipse
     {
     public:
         Circle(const Color &fill, const Point &center, const int &radius);
@@ -51,60 +51,69 @@ namespace svg
         Point center;
         int radius;
     };
-    
-    //############################################################################################################
-    //Implementação da classe Polygon (LM)
-    class Polygon : public SVGElement{
-        public:
-        Polygon(const std::vector <Point> all_points, const Color &fill);
+
+    // ############################################################################################################
+    // Implementação da classe Polygon (LM)
+    class Polygon : public SVGElement
+    {
+    public:
+        Polygon(const std::vector<Point> all_points, const Color &fill);
         void draw(PNGImage &image) const override;
+
     private:
         Color fill;
-        std::vector <Point> all_points;
+        std::vector<Point> all_points;
     };
 
-    //Implementação da classe Rectangle (classe derivada de polygon)(LM)
-    class Rectangle : public Polygon{ 
-        public:
+    // Implementação da classe Rectangle (classe derivada de polygon)(LM)
+    class Rectangle : public Polygon
+    {
+    public:
         Rectangle(const Point &upperL, const int width, const int height, const Color &fill);
 
         void draw(PNGImage &img) const override;
 
-        //função que dá return do vetor all_rect_points - necessário neste formato p/ draw_polygon
-        std::vector<Point> get_rect_coordinates() const { 
-        std::vector<Point> all_rect_points;
-        all_rect_points.push_back(upperL); // Upper Left
-        all_rect_points.emplace_back(upperL.x + width, upperL.y); // Upper Right
-        all_rect_points.emplace_back(upperL.x, upperL.y - height); // Lower Left
-        all_rect_points.emplace_back(upperL.x + width, upperL.y - height); // Lower Right
-        return all_rect_points;}
+        // função que dá return do vetor all_rect_points - necessário neste formato p/ draw_polygon
+        std::vector<Point> get_rect_coordinates() const
+        {
+            std::vector<Point> all_rect_points;
+            all_rect_points.push_back(upperL);                                 // Upper Left
+            all_rect_points.push_back({upperL.x + width, upperL.y});          // Upper Right
+            all_rect_points.push_back({upperL.x, upperL.y - height});         // Lower Left
+            all_rect_points.push_back({upperL.x + width, upperL.y - height}); // Lower Right
+            return all_rect_points;
+        }
 
-        private:
-            Color fill;
-            int width;
-            int height;
-            Point upperL;
+    private:
+        Color fill;
+        int width;
+        int height;
+        Point upperL;
     };
 
-    //Implementação da classe Line 
-    class Line: public SVGElement {
-        public: 
-        Line(double x1, double y1, double x2, double y2, const std::string& stroke);
+    // Implementação da classe Line
+    /* class Line : public SVGElement
+    {
+    public:
+        Line(double x1, double y1, double x2, double y2, const std::string &stroke);
         virtual ~Line();
-        virtual void draw(PNGImage& image) const;
+        virtual void draw(PNGImage &image) const;
+
     private:
         double x1, y1, x2, y2;
         std::string stroke;
     };
-    //Implementação da classe Polyline 
-    class Polyline: public SVGElement {
-        public: 
-        Polyline(const std::vector<Point>& points, const std::string& stroke);
+    // Implementação da classe Polyline
+    class Polyline : public SVGElement
+    {
+    public:
+        Polyline(const std::vector<Point> &points, const std::string &stroke);
         virtual ~Polyline();
-        virtual void draw(PNGImage& image) const;
+        virtual void draw(PNGImage &image) const;
+
     private:
         std::vector<Point> points;
         std::string stroke;
-    };
+    }; */
 }
 #endif
